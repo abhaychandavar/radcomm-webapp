1386d2c38c3ab877649dc6d41857c0b41765fbfb
Abhay Chandavar <abhaychandavar@Abhays-MacBook-Pro.local>
2024-09-04 22:51:28 +0530

    modified website title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
 
       <div className="flex flex-col-reverse gap-0 md:gap-5 md:flex-row md:items-center md:w-[80%]">
         <div className="flex flex-col gap-5 items-center md:items-start p-5 animate-fadeInSlideUp">
-          <h1 className="text-[4rem] md:text-[5rem] mr-0 p-0 text-center md:text-left bg-clip-text bg-gradient-to-r from-primary to-[#a04e00] text-transparent">
-            <strong>R</strong>ad<strong>A</strong>pp
+          <h1 className="text-3xl md:text-7xl mr-0 p-0 text-center md:text-left bg-clip-text bg-gradient-to-r from-primary to-[#a04e00] text-transparent">
+            One platform <br /> for all things DEV
           </h1>
 
           <div className="w-full">
