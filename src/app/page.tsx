4f62ae634c82c52d956c234704201f684bce9354
Abhay Chandavar <abhaychandavar@Abhays-MacBook-Pro.local>
2024-09-04 22:01:53 +0530

    added transitions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import { Button } from "@/components/ui/button";
 import config from "@/config/config";
-import { Mail, Video } from "lucide-react";
+import { Mail } from "lucide-react";
 
 export default function Home() {
   return (
     <main className="p-5 bg-black md:flex md:h-screen md:w-screen md:items-center md:justify-center">
       <div className="hidden md:block absolute top-0 left-0 w-[200px] h-[200px] bg-primary blur-[200px] rounded-lg"></div>
 
-      <div className="flex flex-col-reverse gap-5 md:flex-row md:items-center md:w-[80%]">
-        <div className="flex flex-col gap-5 items-center md:items-start p-5">
+      <div className="flex flex-col-reverse gap-0 md:gap-5 md:flex-row md:items-center md:w-[80%]">
+        <div className="flex flex-col gap-5 items-center md:items-start p-5 animate-fadeInSlideUp">
           <h1 className="text-[4rem] md:text-[5rem] mr-0 p-0 text-center md:text-left bg-clip-text bg-gradient-to-r from-primary to-[#a04e00] text-transparent">
             <strong>R</strong>ad<strong>A</strong>pp
           </h1>
@@ -17,15 +17,13 @@ export default function Home() {
             <p className="text-base text-muted-foreground mb-2 text-center md:text-left">
               Effortlessly build real-time apps, send emails that reach the inbox, and schedule tasks—all in one platform.
             </p>
-            <p className="text-base font-semibold text-muted-foreground text-center md:text-left">
-              Focus on what matters most, and let us handle the rest!
-            </p>
           </div>
           <Button className="w-fit"><Mail className="w-4 h-4 mr-2" />Join waitlist</Button>
         </div>
         <video width="w-1"
-          height="h-1" className="md:w-1/2"
+          height="h-1" className="md:w-1/2 animate-fadeInSlideUp"
           loop={true} muted={true}
+          autoPlay={true}
           controls={false}
           poster="https://vmpn09wz4fdok1nr.public.blob.vercel-storage.com/earthTechPoster-acTGACnQl7YAGCldEYRbYLsAIsBU3t.png"
           preload="auto"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -69,10 +69,21 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        slideUp: {
+          '0%': { transform: 'translateY(20px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        "accordion-down": "accordion-down 0.5s ease-out",
+        "accordion-up": "accordion-up 0.5s ease-out",
+        fadeIn: 'fadeIn 2s ease-in-out',
+        slideUp: 'slideUp 0.5s ease-in-out',
+        fadeInSlideUp: 'fadeIn 0.5s ease-in-out, slideUp 0.5s ease-in-out',
       },
     },
   },
